6d179b5497595b6f12a6d563e54815b57a8a89bb
ViníciusCrisol <vinicius.crisol@etec.sp.gov.br>
2020-08-13 09:33:53 -0300

    Icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,12 @@
-import Head from 'next/head';
-
 import GlobalStyles from '../styles/global';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <link rel='shortcut icon' href='/favicon.ico' />
-      </Head>
-
+    <Layout>
+      <Component {...pageProps} />
       <GlobalStyles />
-
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    </Layout>
   );
 }
 
