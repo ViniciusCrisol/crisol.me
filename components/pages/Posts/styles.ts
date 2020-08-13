4974e951fccfdbf3f153dd0af3775dc7dd3f950c
ViníciusCrisol <vinicius.crisol@etec.sp.gov.br>
2020-08-13 11:51:16 -0300

    Posts organization

--- components/pages/Posts/styles.ts
+++ components/pages/Posts/styles.ts
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin: 0 auto;
   max-width: 710px;
+
+  display: grid;
+  grid-gap: 16px;
+  grid-template-columns: 1fr;
 `;
