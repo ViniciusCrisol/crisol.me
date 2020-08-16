01f36feb58c8f866eab0a66cca7e363262244147
ViníciusCrisol <vinicius.crisol@hotmail.com>
2020-08-16 08:53:44 -0300

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
+
 import Layout from '../components/Layout';
+
 import GlobalStyles from '../styles/global';
+import { initGA, logPageView } from '../lib/analytics';
+
+declare global {
+  interface Window {
+    GA_INITIALIZED: any;
+  }
+}
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }, []);
+
   return (
     <>
       <Head>
